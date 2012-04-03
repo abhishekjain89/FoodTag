ef598b9fd6d9e4e5c805f552d6eca3891b9e59d2
Abhishek Jain <abhishekjain@gatech.edu>
2012-04-03 04:08:29 -0400

    facebook sdk integrated

--- src/com/foodtag/Values.java
+++ src/com/foodtag/Values.java
@@ -13,5 +13,9 @@ import android.util.Log;
 
 
 public class Values extends Application{
+	
+	public static final String APP_ID = "214561815310290";
+	public static final String APP_SECRET = "bca74df0303a11d2b9cadfb1d7b47e16";
+	
 
 }
